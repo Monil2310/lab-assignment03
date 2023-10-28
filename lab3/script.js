26aec65389f68b9e9c0890b2fe0b40a318ca6b6b
Monil Shah <mshah132@hawk.iit.edu>
2023-10-27 23:39:12 -0500

    final commit

--- lab3/script.js
+++ lab3/script.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const billTotalInput = document.getElementById('billTotal');
+    const TotalBillInput = document.getElementById('billTotal');
     const tipSlider = document.getElementById('tipSlider');
     const tipPercentageOutput = document.getElementById('tipPercentage');
-    const tipAmountInput = document.getElementById('tipAmount');
-    const totalWithTipInput = document.getElementById('totalWithTip');
+    const TipAmountInput = document.getElementById('tipAmount');
+    const TotalWithTip = document.getElementById('totalWithTip');
 
     function calculateTip() {
-        const billTotal = parseFloat(billTotalInput.value);
+        console.log("Calculating tip...");
+        const billTotal = parseFloat(TotalBillInput.value);
         const tipPercentage = parseInt(tipSlider.value);
         const tipAmount = billTotal * (tipPercentage / 100);
         const totalWithTip = billTotal + tipAmount;
 
         tipPercentageOutput.textContent = tipPercentage + '%';
-        tipAmountInput.value = tipAmount.toFixed(2);
-        totalWithTipInput.value = totalWithTip.toFixed(2);
+        TipAmountInput.value = tipAmount.toFixed(2);
+        TotalWithTip.value = totalWithTip.toFixed(2);
     }
 
-    billTotalInput.addEventListener('input', calculateTip);
+    TotalBillInput.addEventListener('input', calculateTip);
     tipSlider.addEventListener('input', calculateTip);
 
     // Optionally, added these to test for mobile-specific interactions
-    billTotalInput.addEventListener('change', calculateTip);
+    TotalBillInput.addEventListener('change', calculateTip);
     tipSlider.addEventListener('change', calculateTip);
 
     
